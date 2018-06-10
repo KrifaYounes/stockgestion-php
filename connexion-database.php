b6f419471227521c7ceb096a86433199f60e89fd
Younes <younes.krifa@gmail.com>
2018-06-10 11:05:34 +0200

    update project

---
+++ connexion-database.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Gestion de la base de donnes
+ *
+ */
+class DbConnect {
+
+    private $conn;
+
+    function __construct() {}
+
+    function __destruct() {}
+
+    /**
+     * Establishing database connection
+     */
+    function connect() {
+        try {
+            // On se connecte à MySQL
+            $this->conn = new PDO('mysql:host=localhost;dbname=gestionstock;charset=utf8',
+                'root', 'root',
+                array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+            return $this->conn;
+
+        } catch(Exception $e) {
+            // En cas d'erreur, on affiche un message et on arrête tout
+            die('Erreur : '.$e->getMessage());
+        }
+    }
+
+    function close() {
+
+    }
+
+}
+
+?>
---
+++ get-stock.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Gestion de stock</title>
+    <!-- Bootstrap core CSS-->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom fonts for this template-->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <!-- Page level plugin CSS-->
+    <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin.css" rel="stylesheet">
+</head>
+
+<body class="fixed-nav sticky-footer bg-dark" id="page-top">
+
+<?php include("menu.php"); ?>
+
+
+
+
+
+<div class="content-wrapper">
+    <div class="container-fluid">
+        <!-- Breadcrumbs-->
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+                <a href="#">Stock</a>
+            </li>
+            <li class="breadcrumb-item active">Stock</li>
+        </ol>
+        <!-- Example DataTables Card-->
+        <div class="card mb-3">
+            <div class="card-header">
+                <i class="fa fa-table"></i> Data Table Example</div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Value</th>
+                            <th>Name</th>
+                        </tr>
+                        </thead>
+                        <tfoot>
+                        <tr>
+                            <th>Id</th>
+                            <th>Value</th>
+                            <th>Name</th>
+                        </tr>
+                        </tfoot>
+                        <tbody>
+                        <?php
+
+
+                        require_once __DIR__ . '/connexion-database.php';
+
+
+                        // On se connecte à la base de données
+                        $db = new DbConnect();
+                        $conn = $db->connect();
+
+
+                        // On récupère tout le contenu de la table Stock
+                        $response = $conn->query('SELECT * FROM Stock');
+
+                        // On affiche chaque entrée une à une
+                        while ($data = $response->fetch()) { ?>
+
+                            <tr>
+                                <td> <?php echo $data['id']; ?></td>
+                                <td> <?php echo $data['value']  ; ?></td>
+                                <td> <?php echo $data['name']   ; ?></td>
+                            </tr>
+
+                            <?php
+                        }
+
+                        $reponse->closeCursor(); // Termine le traitement de la requête
+
+                        ?>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+        </div>
+    </div>
+
+    <?php include("footer.php"); ?>
+    <?php include("logout-modal.php"); ?>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <!-- Page level plugin JavaScript-->
+    <script src="vendor/datatables/jquery.dataTables.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.js"></script>
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin.min.js"></script>
+    <!-- Custom scripts for this page-->
+    <script src="js/sb-admin-datatables.min.js"></script>
+</div>
+</body>
+
+</html>
+
+
+
